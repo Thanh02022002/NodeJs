eac5400d81dc460dc860194c713b4206662f96fc
Thanh02022002 <thanhvipad2002@gmail.com>
2024-04-04 17:52:49 +0700

    #25 6commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,27 +1,36 @@
- import db from '../models/index'
+import db from '../models/index';
+import CRUDService from '../services/CRUDService';
 
-let getHomePage = async(req, res) => {
-    
+let getHomePage = async (req, res) => {
+
     try {
         let data = await db.User.findAll();
-        return res.render('homepage.ejs',{
+        return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
     } catch (e) {
-        console.log(e)   
+        console.log(e)
     }
 }
 
-let getAboutPage =(req, res) =>{
+let getAboutPage = (req, res) => {
     return res.render('test/about.ejs')
 }
 
-object: {
-    key: ''
-    value:''
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
 }
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message)
+    return res.send('post crud from server')
+}
+
 
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
