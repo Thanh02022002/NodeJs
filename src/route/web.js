83059d528508d83a763705be431a05e0e8d12684
Thanh02022002 <thanhvipad2002@gmail.com>
2024-05-03 09:25:55 +0700

    #39

--- src/route/web.js
+++ src/route/web.js
@@ -16,7 +16,10 @@ let initWebRoutes = (app) => {
     router.get("/delete-crud", homeController.deleteCRUD);
 
     router.post('/api/login', userController.handleLogin);
-    router.post('/api/get-all-users', userController.handleGetAllUsers);
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+    router.post('/api/create-new-user', userController.handleCreateNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('api/delete-user', userController.handleDeleteUser);  //restAPI
 
     return app.use("/", router);
 }
